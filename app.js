e74782466cbd0c9d2568206aacd31916c332072b
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-16 10:17:13 +0300

    refactor server

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const logger = require('morgan');
 const cors = require('cors');
 const notes = require('./data/notes');
 
 const app = express();
 
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running');
@@ -18,6 +23,15 @@ app.get('/api/notes/:id', (req, res) => {
   const { id } = req.params;
   const note = notes.find(n => n._id === id);
   res.send(note);
+});
+
+app.use((_, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, _, res, __) => {
+  const { code = 500, message = 'Server error' } = err;
+  res.status(code).json({ message });
 });
 
 module.exports = app;
