8569d01376df5beb81914c0999a8352ded5c58d7
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-20 01:06:51 +0300

    remove heroku get users

--- controllers/users.js
+++ controllers/users.js
@@ -80,11 +80,4 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find();
-  res.status(200).json({
-    data: 'users',
-  });
-});
-
-module.exports = { registerUser, authUser, updateUserProfile, getUsers };
+module.exports = { registerUser, authUser, updateUserProfile };
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,5 @@ const protect = require('../middlewares/authMiddleware');
 router.post('/', ctrl.registerUser);
 router.post('/login', ctrl.authUser);
 router.post('/profile', protect, ctrl.updateUserProfile);
-router.get('/all', ctrl.getUsers);
 
 module.exports = router;
